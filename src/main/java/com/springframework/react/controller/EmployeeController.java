f2575fd3181a1326ef6be084c3c04a49baf8f8e0
Muks <msamak@gmail.com>
2020-11-26 18:36:55 -0500

    Added controller with CORS

--- src/main/java/com/springframework/react/controller/EmployeeController.java
+++ src/main/java/com/springframework/react/controller/EmployeeController.java
@@ -3,12 +3,14 @@ package com.springframework.react.controller;
 import com.springframework.react.model.Employee;
 import com.springframework.react.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
