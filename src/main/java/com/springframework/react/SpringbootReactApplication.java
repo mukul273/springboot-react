dcbc37d273037df187b7fc20d6e7f7ac0a1bc485
Muks <msamak@gmail.com>
2020-12-01 11:22:00 -0500

    Added OpenAPI 3.0

--- src/main/java/com/springframework/react/SpringbootReactApplication.java
+++ src/main/java/com/springframework/react/SpringbootReactApplication.java
@@ -1,9 +1,12 @@
 package com.springframework.react;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Employee API", description = "Employee CRUD Service", version = "1.0"))
 public class SpringbootReactApplication {
 
 	public static void main(String[] args) {
